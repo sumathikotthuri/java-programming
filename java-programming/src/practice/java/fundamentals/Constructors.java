92142bd3a0a359d2c9c760aef14a6c7df171e667
sumathikotthuri <kotthuri.sumathi@gmail.com>
2022-12-21 21:35:27 +0530

    sample code for oops concepts

--- java-programming/src/practice/java/fundamentals/Constructors.java
+++ java-programming/src/practice/java/fundamentals/Constructors.java
@@ -2,7 +2,29 @@ package practice.java.fundamentals;
 
 public class Constructors {
     public static void main(String[] args) {
+
+
         Bank b = new Bank();
+
+
+        Chocolate1 choc = new Chocolate1();
+
+        System.out.println("Barcode :"+choc.getBarCode());
+        System.out.println("Name :"+choc.getName());
+        System.out.println("Weight :"+choc.getWeight());
+        System.out.println("Cost :"+choc.getCost());
+
+        choc.setBarCode(102);
+        choc.setName("Hershely\'s");
+        choc.setWeight(24);
+        choc.setCost(50);
+
+        System.out.println("Barcode :"+choc.getBarCode());
+        System.out.println("Name :"+choc.getName());
+        System.out.println("Weight :"+choc.getWeight());
+        System.out.println("Cost :"+choc.getCost());
+
+
     }
 }
 
@@ -10,4 +32,49 @@ class Bank{
     void Bank(){
         System.out.println("null");
     }
+}
+
+class Chocolate1{
+
+    private int barCode;
+    private String name;
+    private int weight;
+    private int cost;
+
+    Chocolate1(){
+        System.out.println("Initializing....");
+        barCode = 101;
+        name = "Cadbury";
+        weight = 12;
+        cost = 10;
+    }
+
+
+
+    public int getBarCode() {
+        return barCode;
+    }
+    public void setBarCode(int barCode) {
+        this.barCode = barCode;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public int getWeight() {
+        return weight;
+    }
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
+    public int getCost() {
+        return cost;
+    }
+    public void setCost(int cost) {
+        this.cost = cost;
+    }
+
+    
 }
